cb1206657dd5d31650027738a704fe8e1c96e2bb
Igor Gerasimov <gerasimowigor@gmail.com>
2026-05-04 21:00:11 +0200

    feat(operator): include LimitRange in cache size ticker (#202)
    Adds LimitRange to the unconditional kinds list in buildCacheSizeKinds so
    the informer_cache_objects gauge reports its cache size, mirroring the
    pattern used for the other 15 unconditional kinds wired in #198.

--- operator/cmd/manager/main.go
+++ operator/cmd/manager/main.go
@@ -650,6 +650,8 @@ func buildCacheSizeKinds(mgr ctrl.Manager, argoPresent bool, logger interface {
 		{kind: "NetworkPolicy", listFactory: func() client.ObjectList { return &networkingv1.NetworkPolicyList{} }},
 		{kind: "ConfigMap", listFactory: func() client.ObjectList { return &corev1.ConfigMapList{} }},
 		{kind: "Secret", listFactory: func() client.ObjectList { return &corev1.SecretList{} }},
+		// Resource governance (#202)
+		{kind: "LimitRange", listFactory: func() client.ObjectList { return &corev1.LimitRangeList{} }},
 		// Cluster-scoped (#159)
 		{kind: "Node", listFactory: func() client.ObjectList { return &corev1.NodeList{} }},
 		{kind: "Namespace", listFactory: func() client.ObjectList { return &corev1.NamespaceList{} }},
